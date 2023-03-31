348deaf00f2447692a0075d7656d1547727c5d60
Александр Галанов <60795996+Galaximum@users.noreply.github.com>
2023-04-01 00:12:01 +0300

    Delete Example directory

--- Example/OSX/ViewController_macOS.swift
+++
@@ -1,9 +0,0 @@
-import Cocoa
-import SwiftyVK
-
-final class ViewController: NSViewController {
-    
-    @IBAction func buttonDown(_ sender: AnyObject) {
-        APIWorker.action(sender.tag)
-    }
-}
